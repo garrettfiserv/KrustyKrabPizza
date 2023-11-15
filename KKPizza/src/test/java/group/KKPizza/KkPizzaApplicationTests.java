4800871daf0fba2ccd626fefb218deb217924e99
Garrett Boscoe <147175589+garrettfiserv@users.noreply.github.com>
2023-11-15 11:42:03 -0500

    add files

---
+++ KKPizza/src/test/java/group/KKPizza/KkPizzaApplicationTests.java
@@ -0,0 +1,13 @@
+package group.KKPizza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KkPizzaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
