4092fd739acbeb362000450bfdf8b2ec39c1096d
Boscoe <garrett.boscoe@fiserv.com>
2023-11-15 16:25:09 -0500

    added makeshift environment variable handling

--- KKPizza/src/main/java/group/KKPizza/KkPizzaApplication.java
+++ KKPizza/src/main/java/group/KKPizza/KkPizzaApplication.java
@@ -3,16 +3,20 @@ package group.KKPizza;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.*;
 import java.sql.*;
-import java.util.Properties;
 
 @SpringBootApplication
 public class KkPizzaApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		SpringApplication.run(KkPizzaApplication.class, args);
-		String jdbcUrl = "jdbc:snowflake://VCB70660.us-east-1.snowflakecomputing.com/?warehouse=COMPUTE_WH&db=KRUSTY_KRAB_PIZZA&schema=PIZZERIA&user=garrettboscoe&password=Fiservrules123&role=ACCOUNTADMIN&CLIENT_RESULT_COLUMN_CASE_INSENSITIVE=true&CLIENT_TIMESTAMP_TYPE_MAPPING=TIMESTAMP_NTZ&JDBC_QUERY_RESULT_FORMAT=JSON";
+		Environment env = new Environment();
+		String pass = env.getPass();
+		String jdbcUrl = "jdbc:snowflake://VCB70660.us-east-1.snowflakecomputing.com/?warehouse=COMPUTE_WH&db=KRUSTY_KRAB_PIZZA&schema=PIZZERIA&user=garrettboscoe&password=" + pass  + "&role=ACCOUNTADMIN&CLIENT_RESULT_COLUMN_CASE_INSENSITIVE=true&CLIENT_TIMESTAMP_TYPE_MAPPING=TIMESTAMP_NTZ&JDBC_QUERY_RESULT_FORMAT=JSON";
 		String selectSQL = "SELECT * FROM KRUSTY_KRAB_PIZZA.PIZZERIA.CUSTOMER";
+
+
 
 		try{
 			Connection connection = DriverManager.getConnection(jdbcUrl);
